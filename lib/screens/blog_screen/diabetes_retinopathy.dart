ce73822014bd4468fbd09dfed987e2f88a632fad
ammar <ammarraneez@gmail.com>
2021-04-19 17:19:11 +0530

    diabetes retinopathy blogs added

--- lib/screens/blog_screen/diabetes_retinopathy.dart
+++ lib/screens/blog_screen/diabetes_retinopathy.dart
@@ -1,6 +1,7 @@
 class DiabetesRetinopathy {
   final titles = [
-    "Risk Factors For Developing Type 2 Diabetes",
+    "How To Prevent Diabetic Retinopathy?",
+    "The Role of Genetics in Diabetic Retinopathy",
   ];
 
   final allBlog = [
@@ -63,11 +64,7 @@ class DiabetesRetinopathy {
   ];
 
   final blogShortDescriptions = [
-    '''
-      A new report indicates that insomnia is a novel risk factor for developing type 2 diabetes. The
-      Sweden-based study that appears in the journal Diabetologia identified 19 risk factors for type
-      2 diabetes. It further evaluated 21 risk factors that had scarce evidence, and another 15 that
-      reduced the risk of the condition.
-    '''
+    '''Diabetic retinopathy is one of the most common complications of diabetes. It’s an eye condition that affects blood vessels in the retina, the innermost layer lining the back of the inside the eye. It occurs due to damage to the small capillaries that supply blood to the light-sensitive retina at the back of the eye...''',
+    '''Diabetic retinopathy is one of the most severe complications that can occur due to the abnormal changes in the microvascular in diabetic patients. Diabetes is known to affect the blood flow to the vital organs including the heart and brain. It can also affect the tiny capillaries supplying blood to the eyes, especially...'''
   ];
 }
