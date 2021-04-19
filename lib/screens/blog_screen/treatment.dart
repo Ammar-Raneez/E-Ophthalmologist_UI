07af63b712e13ef4b9f28c0564fa5976976d45e1
ammar <ammarraneez@gmail.com>
2021-04-19 17:27:50 +0530

    treatment blogs added

--- lib/screens/blog_screen/treatment.dart
+++ lib/screens/blog_screen/treatment.dart
@@ -1,6 +1,9 @@
 class Treatment {
   final titles = [
-    "Risk Factors For Developing Type 2 Diabetes",
+    "What is the Best Treatment For Diabetic Retinopathy?",
+    "How You Can Lose Your Eyesight to Diabetic Retinopathy if Untreated",
+    "How Is Diabetic Retinopathy Diagnosed And Treated?",
+    "What Treatments Are Available for Type 2 Diabetes?"
   ];
 
   final allBlog = [
@@ -63,11 +66,9 @@ class Treatment {
   ];
 
   final blogShortDescriptions = [
-    '''
-      A new report indicates that insomnia is a novel risk factor for developing type 2 diabetes. The
-      Sweden-based study that appears in the journal Diabetologia identified 19 risk factors for type
-      2 diabetes. It further evaluated 21 risk factors that had scarce evidence, and another 15 that
-      reduced the risk of the condition.
-    '''
+    '''Diabetic retinopathy is one of the most common complications caused by diabetes. If left untreated, it may affect the eyesight of the patient resulting in blindness. The latest scientific research studies have led to improved outcomes in the management of this condition. Below is a brief discussion about the latest...''',
+    '''Meet Gede. He is 28, lives in Bandung, Java, Indonesia, and has Type 2 diabetes. Gede lost his sight from Diabetic Retinopathy (DR) or diabetic blindness, which could have been avoided. Like other young men and women with diabetes, he did not think he was at risk and didn’t routinely undertake diabetic eye screenings...''',
+    '''Diabetic retinopathy refers to a condition caused due to the damage to the retina in patients with diabetes. Uncontrolled diabetes can result in the persistently elevated levels of sugar in the blood due to which the light-sensitive part of the eye called the retina is damaged. Early diagnosis and treatment of diabetic...''',
+    '''One of the biggest contributing factors for the onset of type-2 diabetes is your lifestyle, but genetics may also determine if you are more susceptible to the disease. Therefore treatment that may work for someone else may not be the right one for you. Successful management of type-2 diabetes involves targeting both glucose...'''
   ];
 }
