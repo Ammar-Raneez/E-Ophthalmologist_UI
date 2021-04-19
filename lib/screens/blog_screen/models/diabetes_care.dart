866917b0f01dbcddf779d338e31b34694f01d14c
ammar <ammarraneez@gmail.com>
2021-04-19 20:58:52 +0530

    added blog pdfs

--- lib/screens/blog_screen/models/diabetes_care.dart
+++ lib/screens/blog_screen/models/diabetes_care.dart
@@ -1,9 +1,9 @@
 class DiabetesCare {
   final titles = [
+    "Can Stress & Anxiety Cause Diabetes?",
     "Risk Factors For Developing Type 2 Diabetes",
     "A Short History of Diabetes",
     "What You Need to Know About Diabetes and Bones",
-    "Can Stress & Anxiety Cause Diabetes?"
   ];
 
   final allBlog = [
@@ -66,9 +66,9 @@ class DiabetesCare {
   ];
 
   final blogShortDescriptions = [
+    '''Everyone experiences stress at some point in their lives that can result in worry, anxiety, and tension. Stress affects everyone to some degree, but it may be more difficult to manage when people learn that they have diabetes. Diabetes is a relentless disease that requires constant attention, awareness, and decision-making...''',
     '''A new report indicates that insomnia is a novel risk factor for developing type 2 diabetes. The Sweden-based study that appears in the journal Diabetologia identified 19 risk factors for type 2 diabetes. It further evaluated 21 risk factors that had scarce evidence, and another 15 that reduced the risk of the condition...''',
     ''' Modern science has enabled us to diagnose and manage diabetes pretty effectively, but where did it all start? Diabetes has been documented by scientists for thousands of years, under one name or another, and the very first known mention of the disease was by an Egyptian physician called Hesy-Ra in 1552 B.C. He observed...''',
     '''It ́s estimated that worldwide an osteoporotic fracture occurs every 3 seconds, which amounts to almost 25 000 fractures per day or 9 million per year. Sadly, it ́s quite likely to be a person living with diabetes as research shows that they are three times more likely to break a bone than persons how don’t have diabetes...''',
-    '''Everyone experiences stress at some point in their lives that can result in worry, anxiety, and tension. Stress affects everyone to some degree, but it may be more difficult to manage when people learn that they have diabetes. Diabetes is a relentless disease that requires constant attention, awareness, and decision-making...'''
   ];
 }
--- lib/screens/blog_screen/models/treatment.dart
+++ lib/screens/blog_screen/models/treatment.dart
@@ -1,8 +1,8 @@
 class Treatment {
   final titles = [
+    "How Is Diabetic Retinopathy Diagnosed And Treated?",
     "What is the Best Treatment For Diabetic Retinopathy?",
     "How You Can Lose Your Eyesight to Diabetic Retinopathy if Untreated",
-    "How Is Diabetic Retinopathy Diagnosed And Treated?",
     "What Treatments Are Available for Type 2 Diabetes?"
   ];
 
@@ -66,9 +66,9 @@ class Treatment {
   ];
 
   final blogShortDescriptions = [
+    '''Diabetic retinopathy refers to a condition caused due to the damage to the retina in patients with diabetes. Uncontrolled diabetes can result in the persistently elevated levels of sugar in the blood due to which the light-sensitive part of the eye called the retina is damaged. Early diagnosis and treatment of diabetic...''',
     '''Diabetic retinopathy is one of the most common complications caused by diabetes. If left untreated, it may affect the eyesight of the patient resulting in blindness. The latest scientific research studies have led to improved outcomes in the management of this condition. Below is a brief discussion about the latest...''',
     '''Meet Gede. He is 28, lives in Bandung, Java, Indonesia, and has Type 2 diabetes. Gede lost his sight from Diabetic Retinopathy (DR) or diabetic blindness, which could have been avoided. Like other young men and women with diabetes, he did not think he was at risk and didn’t routinely undertake diabetic eye screenings...''',
-    '''Diabetic retinopathy refers to a condition caused due to the damage to the retina in patients with diabetes. Uncontrolled diabetes can result in the persistently elevated levels of sugar in the blood due to which the light-sensitive part of the eye called the retina is damaged. Early diagnosis and treatment of diabetic...''',
     '''One of the biggest contributing factors for the onset of type-2 diabetes is your lifestyle, but genetics may also determine if you are more susceptible to the disease. Therefore treatment that may work for someone else may not be the right one for you. Successful management of type-2 diabetes involves targeting both glucose...'''
   ];
 }
