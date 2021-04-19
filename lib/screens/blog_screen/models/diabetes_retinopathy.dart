388034982cc1f7a74131bf0e0e8c5c4b107b6b5a
ammar <ammarraneez@gmail.com>
2021-04-19 21:02:18 +0530

    added path to respective pdfs in each model

--- lib/screens/blog_screen/models/diabetes_retinopathy.dart
+++ lib/screens/blog_screen/models/diabetes_retinopathy.dart
@@ -5,62 +5,8 @@ class DiabetesRetinopathy {
   ];
 
   final allBlog = [
-    ''' 
-      A new report indicates that insomnia is a novel risk factor for developing type 2 diabetes. The
-      Sweden-based study that appears in the journal Diabetologia identified 19 risk factors for type
-      2 diabetes. It further evaluated 21 risk factors that had scarce evidence, and another 15 that
-      reduced the risk of the condition.
-      Researchers have long identified factors that increase the risk of developing type 2 diabetes.
-      These factors include family history, weight, fat distribution, inactivity, age and even race. Other
-      suggestive risk factors that have been mentioned but may need to be further studied include
-      alcohol consumption, skipping breakfast, daytime napping, anxiety disorders, urinary sodium,
-          certain amino acids and inflammatory factors, and lack of sleep.
-      Obesity is still the predominant risk factor for type 2 diabetes. But insomnia was the headline
-      maker in the Swedish study — people living with the condition are 17% more likely to develop
-      type 2 diabetes than those without it. Insomnia was also seen as an element in the association
-      between depression and type 2 diabetes.
-      The team used a method called ‘Mendelian randomization’ (MR) to obtain their findings. This
-      technique blends genetic information and conventional epidemiological methods. It also
-      addresses questions related to causality without biases, that could compromise the validity of
-      epidemiological approaches.
-      The Swedish researchers used data from the Diabetes Genetics Replication and Meta-analysis
-      consortium. They evaluated 74,124 cases of type 2 diabetes and 824,006 control participants
-      with European ancestry for the study population. The participants’ mean age was around 55
-      years, and 51.8% of them were male. The researchers also screened 238 studies before including
-      40 individual papers in their investigation. Among the 97 factors they looked at, only 19
-      increased diabetes risk.
-      Daytime napping also appeared to be a risk factor for type 2 diabetes. However, because it’s
-      strongly related to insomnia, it’s unclear whether daytime napping is an independent risk factor.
-      Other risk factors found include:
-      • depression
-      • smoking
-      • high blood pressure
-      • caffeine consumption
-      The exposures associated with a decreased risk of type 2 diabetes include:
-      • the amino acid, alanine
-      • high-density lipoproteins, or good cholesterol
-      • total cholesterol
-      • the age when females start menstruating
-      • testosterone levels
-      These findings should inform public health policies for the primary prevention of type 2
-      diabetes. These measures could include lowering obesity and smoking rates and improving
-      mental health, sleep quality, educational level and birth weight.
-      More work is needed in this field, especially in less homogenous populations than Sweden’s,
-      and since major risk factors for type 2 diabetes differ by ancestry. Studies have also found that
-      the chance of developing diabetes is significantly higher for Black people — around 66 more
-      cases of diabetes per 1,000 people — compared with white adults.
-      Ballooning diabetes numbers around the world scream for more testing. According to the
-      
-      International Diabetes Foundation (IDF), 79% of adults with diabetes live in low- and middle-
-      income countries. In Southeast Asia, for example, the IDF says some 88 million adults live with
-      
-      diabetes, with over half undiagnosed. By 2045, the global diabetic population is expected to
-      exceed 150 million.
-      South and Central America are the two regions presenting an increased mortality trend with
-      relation to diabetes. The IDF estimated the average prevalence of diabetes there at 8.0%; this
-      is expected to rise to 9.8% by 2045. This underscores the importance of analyzing all the risk
-      factors influencing the disease and putting preventative measures in place.
-    '''
+    "blogs/retinopathy 0.pdf",
+    "blogs/retinopathy 1.pdf",
   ];
 
   final blogShortDescriptions = [
