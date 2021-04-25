44cfad6eb2e3c930ecbd04af68e4e324a6885fd0
ammar <ammarraneez@gmail.com>
2021-04-25 19:49:39 +0530

    show message if no documents have been added

--- lib/screens/reports_and_appointment/edit_report_screen.dart
+++ lib/screens/reports_and_appointment/edit_report_screen.dart
@@ -277,6 +277,7 @@ class _EditReportScreenState extends State<EditReportScreen> {
                         SizedBox(
                           height: 30,
                         ),
+                        enableTextFields ?
                         Column(
                           children: imageDocumentsURLS.length != 0
                               ? List.generate(
@@ -308,6 +309,22 @@ class _EditReportScreenState extends State<EditReportScreen> {
                                     height: 300,
                                   ),
                                 ),
+                        ) : Container(
+                          height: MediaQuery.of(context).size.height / 2,
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "No Associated documents have been added",
+                                style: kTextStyle.copyWith(
+                                  fontSize: 30
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
                         ),
                         SizedBox(
                           height: 40,
