c0bf6ed950097aaf63a643af3ee54f122c5a4dc9
ammar <ammarraneez@gmail.com>
2021-04-26 14:35:38 +0530

    text format in diagnosis result screen

--- lib/screens/diagnosis_result_screen.dart
+++ lib/screens/diagnosis_result_screen.dart
@@ -14,32 +14,14 @@ class _DiagnosisResultScreenState extends State<DiagnosisResultScreen> {
   Map insights = {
     "No Diabetic Retinopathy":
         "Continue regular scanning and monitoring to prevent form sight threatening retinopathy. Keep your blood sugar, blood pressure, and cholesterol levels under control.",
-    "Mild": """
-      You've been diagnosed with the first stage of retinopathy also called background retinopathy
-      You probably won't have vision problems, so you may not need treatment. 
-      But consult your doctor about ways to keep your condition from getting worse.
-      Keep your blood sugar, blood pressure, and cholesterol levels under control.
-      If the condition is diagnoses in both eyes,
-      you have a 25% chance of progressing to the third stage in the next 3 years.
-    """,
-    "Moderate": """
-      You've been diagnosed with the second stage of retinopathy also called pre-proliferative retinopathy
-      At this stage, the blood vessels in your retinas swell. Physical changes are caused to the retina.
-      Reaching this stage means a greater chance that the disease will affect your vision. 
-      Consult your ophthalmologist or optometrist; they may recommend eye tests every 3 to 6 months.
-    """,
-    "Severe": """
-      This is also called proliferative retinopathy. In this stage, your blood vessels become even more blocked. 
-      There’s a very high chance that you’ll lose your vision. Treatment may be able to stop further vision loss. 
-      But if you’ve already lost some of your vision, it’s unlikely to come back.
-      Consult your ophthalmologist or optometrist on future clarification.
-    """,
-    "Proliferative": """"
-      You are diagnosed with the advanced stage of retinopathy.
-      Neovascularization happened due to the growth of new blood vessels in your retinas.
-      Due to retinal detachment, there is a high chance of permanent loss of both straight-ahead and side vision. 
-      Consult your ophthalmologist or optometrist immediately to get the necessary treatment.
-    """
+    "Mild":
+        "You've been diagnosed with the first stage of retinopathy also called background retinopathy. You probably won't have vision problems, so you may not need treatment. But consult your doctor about ways to keep your condition from getting worse. Keep your blood sugar, blood pressure, and cholesterol levels under control. If the condition is diagnoses in both eyes, you have a 25% chance of progressing to the third stage in the next 3 years.",
+    "Moderate":
+        "You've been diagnosed with the second stage of retinopathy also called pre-proliferative retinopathy. At this stage, the blood vessels in your retinas swell. Physical changes are caused to the retina. Reaching this stage means a greater chance that the disease will affect your vision. Consult your ophthalmologist or optometrist; they may recommend eye tests every 3 to 6 months.",
+    "Severe":
+        "This is also called proliferative retinopathy. In this stage, your blood vessels become even more blocked. There’s a very high chance that you’ll lose your vision. Treatment may be able to stop further vision loss. But if you’ve already lost some of your vision, it’s unlikely to come back. Consult your ophthalmologist or optometrist on future clarification.",
+    "Proliferative":
+        "You are diagnosed with the advanced stage of retinopathy. Neovascularization happened due to the growth of new blood vessels in your retinas. Due to retinal detachment, there is a high chance of permanent loss of both straight-ahead and side vision. Consult your ophthalmologist or optometrist immediately to get the necessary treatment."
   };
 
   @override
